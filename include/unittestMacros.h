8d63e027751b0575edbf3961b45a74c2b3e96ed2
Johannes Teichrieb <teichrieb@hameg.com>
2015-04-16 13:13:05 +0200

    Add include guard to unittestMacros.h

--- include/unittestMacros.h
+++ include/unittestMacros.h
@@ -1,4 +1,12 @@
+#ifndef _UNITTEST_MACROS
+#define _UNITTEST_MACROS
+
+#ifdef ASSERT
+#undef ASSERT
+#endif
 
 #define ASSERT(COND) if (!(COND)) return -1
 // _WEC - With Error Code
 #define ASSERT_WEC(COND, EC) if (!(COND)) return (EC) ? (EC) : -1
+
+#endif
